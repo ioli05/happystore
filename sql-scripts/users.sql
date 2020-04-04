652557b2b7ee08b94bffb9e2bcc3166a07e37f94
Nicuta Loredana Ionela <ionela.nicuta05@gmail.com>
2020-04-04 18:01:36 +0300

    Modified dockrfile

--- sql-scripts/users.sql
+++ sql-scripts/users.sql
@@ -7,3 +7,6 @@ password varchar(24)
 INSERT INTO users (guid, username, password)
 VALUES (1, 'ionela', 'parola'),
        (2, 'alex', 'parola2');
+
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'admin';
+flush privileges;      
